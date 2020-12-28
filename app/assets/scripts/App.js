6c9211f866bdd03b9e85f07a4b2e86c46993c793
Shin <scho524@gmail.com>
2020-12-27 20:42:37 -0600

    completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,8 +6,26 @@ import StickyHeader from './modules/StickyHeader'
 new StickyHeader();
 new RevealOnScroll('.feature-item');
 new RevealOnScroll('.testimonial');
-const mobileMenu = new MobileMenu();
+new MobileMenu();
+let modal;
 
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if (typeof modal === "undefined") {
+            import(/* webpackChunkName: "modal" */ './modules/Modal')
+                .then((module) => {
+                    modal = new module.default();
+                    setTimeout(() => {
+                        modal.openTheModal()
+                    }, 20)
+                })
+                .catch(() => console.log('Problem loading modal module'))
+        } else {
+            modal.openTheModal();
+        }
+    })
+})
 
 if (module.hot) {
     module.hot.accept()
